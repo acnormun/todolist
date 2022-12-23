ac1aa6f148630a29eca9b1d30fb3dfdd9efa2a44
Ana Clara <anaclaranoronha.m@gmail.com>
2022-12-23 16:13:20 -0300

    finished CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
+import { TodoeditComponent } from './todo/todoedit/todoedit.component';
 import { TododeleteComponent } from './todo/tododelete/tododelete.component';
 
 import { TodocreateComponent } from './todo/todocreate/todocreate.component';
 import { TodolistComponent } from './todo/todolist/todolist.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: 'todolist', pathMatch:'full'},
   {path: "todolist", component: TodolistComponent},
   {path: 'todocreate', component: TodocreateComponent},
-  {path: 'todos/tododelete/:id', component: TododeleteComponent}
+  {path: 'todos/tododelete/:id', component: TododeleteComponent},
+  {path: 'todos/todoedit/:id', component: TodoeditComponent}
 ];
 
 @NgModule({
--- src/app/todo/todoedit/todoedit.component.ts
+++ src/app/todo/todoedit/todoedit.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TodoserviceService } from './../../todoservice.service';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todoedit',
   templateUrl: './todoedit.component.html',
   styleUrls: ['./todoedit.component.css']
 })
-export class TodoeditComponent {
+export class TodoeditComponent implements OnInit{
+
+  todo: Todo = {
+    id: 0,
+    conteudo: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: TodoserviceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+      const id = this.route.snapshot.paramMap.get('id');
+      this.service.search(parseInt(id!)).subscribe((todo)=>{
+        this.todo = todo;
+      })
+  }
+
+  editTodo(){
+    this.service.edit(this.todo).subscribe(()=>{
+      this.router.navigate(['/todolist'])
+    })
+  }
+
+  cancel(){
+    this.router.navigate(['/todolist'])
+  }
 
 }
